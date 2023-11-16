31a317dc899082a1b720fd6ca41e682e779b8e18
epetitgirard <emmanuel.petitgirard@docaposte.fr>
2023-11-16 19:32:27 +0100

    add security management

---
+++ security/tokenManagement.mjs
@@ -0,0 +1,42 @@
+import jwt from "jsonwebtoken";
+
+export function authenticateToken(req, res, next) {
+
+    // Read the JWT access token from the authorization header
+    const accessToken = (req.headers && req.headers.authorization) && req.headers.authorization.replace("Bearer ", "");
+  
+    jwt.verify(
+      accessToken,
+      process.env.USERFRONT_PUBLIC_KEY,
+      { algorithms: ["RS256"] }, async (err, auth) => {
+        if (err) {
+          res.send("Error verifying").status(403);
+        } else {
+          next();     
+        } 
+      }
+    );
+  }
+
+  export function authenticateTokenAdmin(req, res, next) {
+
+    // Read the JWT access token from the authorization header
+    const accessToken = (req.headers && req.headers.authorization) && req.headers.authorization.replace("Bearer ", "");
+  
+    jwt.verify(
+      accessToken,
+      process.env.USERFRONT_PUBLIC_KEY,
+      { algorithms: ["RS256"] }, async (err, auth) => {
+        if (err) {
+          res.send("Error verifying").status(403);
+        } else {
+          const roles = auth.authorization["zn5p5xvb"].roles;
+          if (!roles.includes("admin")) {
+            res.send("Unauthorized").status(401);
+          } else {
+            next();
+          }
+        } 
+      }
+    );
+  } 
